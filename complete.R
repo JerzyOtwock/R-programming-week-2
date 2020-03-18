46c78541e216d1777853ece0453826e5ccca0b71
unknown <jerzy.kwiatkowski@yahoo.pl>
2020-03-18 13:39:25 +0100

    Ver. 1.0

---
+++ complete.R
@@ -0,0 +1,33 @@
+
+complete <- function(directory , id = c(2,4) ){
+    library(data.table)
+    # do zmiany adres na git
+    # directory <- "specdata"
+    fullPath <-  paste0(getwd(),"\\",directory,"\\")
+    
+    # fullPath <- ('H:\\LITERATURA_i_POBRANE\\R_kurs\\Cursera R\\specdata\\')
+       
+    #### doklejam zera do nazwy
+    
+    data_analysis <- data.table()
+    # data_analysis <- read.csv2('H:\\LITERATURA_i_POBRANE\\R_kurs\\Cursera R\\specdata\\001.csv', sep = ',' , dec =".")
+    
+    for(i in id){
+        fileName <- substr(paste0("000", i,".csv"), nchar( paste0("000", i,".csv")) - 6 ,nchar( paste0("000", i,".csv")))
+        tmp <- read.csv2(paste0(fullPath, fileName), sep = ',' , dec ="." )
+        data_analysis <- rbind(  data_analysis, tmp)
+    }
+    
+    #### wylicznaki  
+
+    # data_analysis[ID %in% id,]
+    data_analysis <- data.table(na.omit(data_analysis))
+    final <- data_analysis[ ,.( nobs = table(data_analysis$ID))]
+ 
+    
+    return(final)
+    
+}
+
+source("complete.R")
+complete("specdata")
